c6cf27dcee2cfddec196a71e6dce621689e66137
fussy113 <s1220080@gmail.com>
2018-02-24 23:50:37 +0900

    update rooms

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,2 +1,3 @@
 class Room < ApplicationRecord
+	belongs_to :user ,class_name: "User", foreign_key: "create_user_id"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180224143244) do
+ActiveRecord::Schema.define(version: 20180224144243) do
 
   create_table "rooms", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "name", null: false
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20180224143244) do
     t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "create_user_id"
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
