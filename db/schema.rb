05113545ba4f29b87b5adf2e23e0e9c16e0345bc
fussy113 <s1220080@gmail.com>
2018-02-26 02:53:12 +0900

    run db:migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180224145504) do
+ActiveRecord::Schema.define(version: 20180224151027) do
+
+  create_table "imagelogs", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer "room_id"
+    t.integer "user_id"
+    t.string "url"
+    t.string "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "rooms", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "name", null: false
