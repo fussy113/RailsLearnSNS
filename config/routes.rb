fabe1e2bfbb362b8442eb99f3d04112f7f6e802f
fussy113 <s1220080@gmail.com>
2018-02-26 03:49:27 +0900

    Add Users Controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => {
+    :sessions => 'users/sessions',
+    :passwords => 'users/passwords',
+    :registations => 'users/resistrations'
+  }
   root to: 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
