4b7d5b55c9c6088846b5f649dbb370017b728a8f
fussy113 <s1220080@gmail.com>
2018-02-26 05:12:25 +0900

    update new method

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,8 +1,22 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
-  # before_action :configure_sign_up_params, only: [:create]
-  # before_action :configure_account_update_params, only: [:update]
+  before_action :configure_params
+
+  before_action :create, only: [:complete]
+
+  def confirm
+	  @user = User.new(sign_up_params)
+	  if @user.valid?
+		  render :action => 'confirm'
+	  else
+		  render :action => 'new'
+	  end
+  end
+
+  def complete
+	  render :action => 'complete'
+  end
 
   # GET /resource/sign_up
   # def new
@@ -41,14 +55,12 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # protected
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_up_params
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
-  # end
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_account_update_params
-  #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
-  # end
+  def configure_params
+	  added_attrs = [ :name, :email, :password, :password_confirmation　]
+	  devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+	  devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+	  devise_parameter_sanitizer.permit :sign_in, keys: added_attrs
+  end
 
   # The path used after sign up.
   # def after_sign_up_path_for(resource)
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable ,:confirmable
   has_many :rooms
   has_many :textlogs
   has_many :imagelogs
