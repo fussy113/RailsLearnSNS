8d6962d63ff656aaa7e691248eaceb5051522b6d
fussy113 <s1220080@gmail.com>
2018-02-26 03:51:22 +0900

    fix typo

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => {
     :sessions => 'users/sessions',
     :passwords => 'users/passwords',
-    :registations => 'users/resistrations'
+    :registrations => 'users/registrations'
   }
   root to: 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
