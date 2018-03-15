c040ed5cad6f5dd4081c876cb40729824d90ded6
fussy113 <s1220080@gmail.com>
2018-03-15 13:36:54 +0900

    add rooms_controller method
    rooms_controllerの各メソッド、ストロングパラメータを作成

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,2 +1,28 @@
 class RoomsController < ApplicationController
+  def index
+
+  end
+  def show
+
+  end
+  def new
+
+  end
+  def create
+
+  end
+  def edit
+
+  end
+  def update
+
+  end
+  def destroy
+
+  end
+
+  private
+    def params_room
+      params.require(:rooms).permit(:name, :description, :password, :created_user_id)
+    end
 end
