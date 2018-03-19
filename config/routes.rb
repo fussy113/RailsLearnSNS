55ae8c50a10e9c313043fc545212a5b2561de34c
fussy113 <s1220080@gmail.com>
2018-03-19 16:35:24 +0900

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,16 @@ Rails.application.routes.draw do
     post 'users/sign_up/complete' => 'users/registrations#complete'
   end
 
-  resource :rooms
+  scope '/rooms' do
+    get '/' => 'rooms#index'
+    get '/:id' => 'rooms#show'
+    get '/new' => 'rooms#new'
+    post '/' => 'rooms#create'
+    get '/:id/edit' => 'rooms#edit'
+    put '/:id' => 'rooms#update'
+    delete '/:id' => 'rooms#destroy'
+    post '/:id/confirm' => 'rooms#confirm'
+  end
 
   root to: 'rooms#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
