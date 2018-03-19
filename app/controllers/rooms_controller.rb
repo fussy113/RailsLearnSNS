1653d330abfdeb033f7526d972b8832c750f993c
fussy113 <s1220080@gmail.com>
2018-03-19 21:34:36 +0900

    update method
    confirm以外のmethodの完成

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,36 +1,48 @@
 class RoomsController < ApplicationController
   def index
-
+    @rooms = Room.all
   end
+
   def show
-
+    @room = Room.find_by_id(params[:id])
   end
+
   def new
     @room = Room.new
   end
+
   def create
-    binding.pry
     @room = Room.new(room_params)
-    @room.create_user_id = current_user.id
     if @room.save
-      redirect_to rooms_path
+      redirect_to root_path
     else
-      render new_room_path
+      redirect_to '/rooms/new' , method: :get
     end
-
   end
+
   def edit
-    @room = Room.find(params[:id])
+    @room = Room.find_by_id(params[:id])
   end
+
   def update
-
+    @room = Room.find_by_id(params[:id])
+    @room.update_attributes(room_params)
+    redirect_to '/rooms/:id' , :id => @room.id
   end
-  def destroy
 
+  def destroy
+    @room = Room.find_by_id(params[:id]).delete
+    redirect_to '/rooms/'
+  end
+
+
+  def confirm
+    @room = Room.find_by_id(params[:id])
   end
 
   private
     def room_params
-      params.require(:room).permit(:name, :description, :password)
+      params.require(:room).permit(:name, :description, :password,:create_user_id)
     end
+
 end
