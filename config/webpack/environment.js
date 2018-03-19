1d1d71d59a0573509bfb358594b0c16540b50c19
fussy113 <s1220080@gmail.com>
2018-03-20 02:37:40 +0900

    change include
    js,cssの読み込み先を変更

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,5 +1,17 @@
 const { environment } = require('@rails/webpacker')
+const webpack = require('webpack')
 const vue =  require('./loaders/vue')
+
+environment.plugins.set(
+    'Provide',
+    new webpack.ProvidePlugin({
+        $: 'jquery',
+        jQuery: 'jquery',
+        Tether: 'tether',
+        'window.Tether': 'tether',
+        Popper: ['popper.js', 'default']
+    })
+)
 
 environment.loaders.append('vue', vue)
 module.exports = environment
