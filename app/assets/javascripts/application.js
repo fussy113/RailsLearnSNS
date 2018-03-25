a24379f9b08a5f4932af97bbb7123f1cd94f4ee3
fussy113 <s1220080@gmail.com>
2018-03-25 18:02:58 +0900

    import action cable

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,9 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require rails-ujs
+
 //= require turbolinks
 //= require_tree .
 //= require material
+//= action_cable
+var cable = ActionCable.createConsumer("/cable");
