14239ba370a25c87e59ad1829f44e9d363bbdf92
fussy113 <s1220080@gmail.com>
2018-03-26 13:57:12 +0900

    remove line
    今回はjobを利用しないので2行目をコメント

--- app/models/textlog.rb
+++ app/models/textlog.rb
@@ -1,5 +1,5 @@
 class Textlog < ApplicationRecord
-	after_create_commit {BroadCastMessageJob.perform_later self}
+	# after_create_commit {BroadCastMessageJob.perform_later self}
 	belongs_to :user ,class_name: "User"
 	belongs_to :room ,class_name: "Room"
 end
