0aac5694b3220c0f6ce6149a2ebd517aa5a48eb0
fussy113 <s1220080@gmail.com>
2018-03-26 17:43:13 +0900

    update config
    fog_providerを追記しないと動かないので修正

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,9 +1,10 @@
 CarrierWave.configure do |config|
   # s3の設定を記述 アクセスキー、シークレットキーを直書きしてgitにあげないよう注意
+  config.fog_provider = 'fog/aws'
   config.fog_credentials = {
     provider: 'AWS',
-    aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-    aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+    aws_access_key_id: ENV['S3_ACCESS_KEY_ID'],
+    aws_secret_access_key: ENV['S3_SECRET_ACCESS_KEY'],
     region: 'ap-northeast-1'
   }
 
