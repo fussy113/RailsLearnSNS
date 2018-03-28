d7b8a7ceb18cc8757e9b52fb82455568818cf364
fussy113 <s1220080@gmail.com>
2018-03-28 18:41:17 +0900

    update logs models
    textlogの更新時に同時にimagelogを更新するようmodelを修正

--- app/models/imagelog.rb
+++ app/models/imagelog.rb
@@ -1,3 +1,4 @@
 class Imagelog < ApplicationRecord
+	mount_uploader :image, ImageUploader
 	belongs_to :textlog ,class_name: "Textlog"
 end
--- app/models/textlog.rb
+++ app/models/textlog.rb
@@ -3,4 +3,5 @@ class Textlog < ApplicationRecord
 	belongs_to :user ,class_name: "User"
 	belongs_to :room ,class_name: "Room"
 	has_one :imagelog , dependent: :destroy
+	accepts_nested_attributes_for :imagelog
 end
